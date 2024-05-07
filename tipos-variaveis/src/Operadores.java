0fcccda66c128f97b3c720fcb9ad580b321fe938
Luciano <lucsl21@hotmail.com>
2024-05-07 20:36:44 -0300

    exemplos operador ternario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,7 +2,7 @@ public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
         numero = - numero;
-        System.out.println(-numero);
+        System.out.println(numero);
         numero = numero * -1;
         System.out.println(numero);
 
