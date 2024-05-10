c24733b181cec8cd980b5a5cd2e6449e720f2674
Luciano <lucsl21@hotmail.com>
2024-05-10 20:55:31 -0300

    primeiro commit controle-fluxo

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+/*public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado"; //condição ternaria
+		System.out.println(resultado);  
+    }
+}
+*/
+
+// Cenário 2
+public class ResultadoEscolar {
+	public static void main(String[] args) {
+		int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+	}
+}
