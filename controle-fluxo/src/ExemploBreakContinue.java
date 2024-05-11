de5e10a1781c19c244bbb831ef755971142a0ee0
Luciano <lucsl21@hotmail.com>
2024-05-11 17:53:31 -0300

    extruturas de repeticao

---
+++ controle-fluxo/src/ExemploBreakContinue.java
@@ -0,0 +1,31 @@
+// class ExemploBreakContinue continue
+public class ExemploBreakContinue {
+	public static void main(String[] args) {
+	
+	for(int numero = 1; numero <=5; numero ++){
+		if(numero==3)
+			continue;
+		
+		System.out.println(numero);
+		
+	}
+
+    }
+}
+
+/*
+// class ExemploBreakContinue break
+public class ExemploBreakContinue {
+	public static void main(String[] args) {
+	
+	for(int numero = 1; numero <=5; numero ++){
+		if(numero==3)
+			break;
+		
+		System.out.println(numero);
+		
+	}
+
+    }
+}
+ */
